5dd8fc39c3aafa17839a9f83a3429d5c2768ccac
payel15-5106 <payel15-5106@diu.edu.bd>
2025-02-22 04:52:07 +0000

    Cached Network

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,17 +22,28 @@ class day_3 extends StatelessWidget {
         backgroundColor: Colors.red,
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Image(
-                image: NetworkImage(
-                  "https://www.shutterstock.com/image-vector/abstract-background-random-numbers-gray-260nw-244169761.jpg",
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 10,
+              children: [
+                Image(
+                  image: NetworkImage(
+                    "https://www.shutterstock.com/image-vector/abstract-background-random-numbers-gray-260nw-244169761.jpg",
+                  ),
                 ),
-              ),
-              Image.network(
-                "https://media.assettype.com/theceo%2Fimport%2F2020%2F08%2FAll-about-Random-Number-Generators-RNGs-and-its-use-cases.jpg",
-              ),
-            ],
+                Image.network(
+                  "https://media.assettype.com/theceo%2Fimport%2F2020%2F08%2FAll-about-Random-Number-Generators-RNGs-and-its-use-cases.jpg",
+                  fit: BoxFit.fitWidth,
+                ),
+                CachedNetworkImage(
+                  imageUrl:
+                      "https://www.shutterstock.com/image-photo/chameleon-close-eye-portrait-reptile-600nw-2466925375.jpg",
+                  fit: BoxFit.fitWidth,
+                ),
+              ],
+            ),
           ),
         ),
       ),
