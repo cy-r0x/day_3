9d2a817e3a998a29ee72dde684ba507d48b7bf8b
payel15-5106 <payel15-5106@diu.edu.bd>
2025-02-22 06:43:27 +0000

    Nav-Drawer with Stateful Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,106 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const day_3());
 }
 
-class day_3 extends StatelessWidget {
+class day_3 extends StatefulWidget {
   const day_3({super.key});
+  @override
+  day3State createState() => day3State();
+}
 
-  // This widget is the root of your application.
+class day3State extends State<day_3> {
+  int idx = 0;
+
+  List<Widget> pages = [Text("Home"), Text("Search"), Text("Settings")];
+
+  void onClick(int idx) {
+    setState(() {
+      this.idx = idx;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Advance UI",
       home: Scaffold(
+        drawer: Drawer(
+          backgroundColor: Colors.blue,
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  duration: Duration(milliseconds: 1000),
+                  child: Row(
+                    spacing: 10,
+                    children: [
+                      Icon(Icons.people, color: Colors.white, size: 24),
+                      Text("Contact", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home, color: Colors.white),
+                title: Text(
+                  "Home",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                hoverColor: const Color.fromARGB(255, 223, 223, 223),
+                textColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.home, color: Colors.white),
+                title: Text(
+                  "Home",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                hoverColor: const Color.fromARGB(255, 223, 223, 223),
+                textColor: Colors.white,
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.home, color: Colors.white),
+                title: Text(
+                  "Home",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                hoverColor: const Color.fromARGB(255, 223, 223, 223),
+                textColor: Colors.white,
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text("Advance UI"),
           backgroundColor: Colors.blueAccent,
           titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
         ),
-        backgroundColor: Colors.red,
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 10,
-              children: [
-                Image(
-                  image: NetworkImage(
-                    "https://www.shutterstock.com/image-vector/abstract-background-random-numbers-gray-260nw-244169761.jpg",
-                  ),
-                ),
-                Image.network(
-                  "https://media.assettype.com/theceo%2Fimport%2F2020%2F08%2FAll-about-Random-Number-Generators-RNGs-and-its-use-cases.jpg",
-                  fit: BoxFit.fitWidth,
-                ),
-                CachedNetworkImage(
-                  imageUrl:
-                      "https://www.shutterstock.com/image-photo/chameleon-close-eye-portrait-reptile-600nw-2466925375.jpg",
-                  errorWidget:
-                      (context, url, error) => Image.network(
-                        "https://static.vecteezy.com/system/resources/thumbnails/012/042/292/small_2x/warning-sign-icon-transparent-background-free-png.png",
-                      ),
-                ),
-              ],
-            ),
+        backgroundColor: Colors.blueGrey,
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [pages[idx]],
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onClick,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Setting",
+            ),
+          ],
         ),
       ),
     );
